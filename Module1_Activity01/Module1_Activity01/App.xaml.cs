020e6e769c0f8597dada5d806a56e4f9c5a8c413
Troy <Troy@DESKTOP-JLCV8UI>
2022-09-11 23:44:45 +0800

    Add project files.

---
+++ Module1_Activity01/Module1_Activity01/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Module1_Activity01
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Module1_Activity01/Module1_Activity01/MainClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Module1_Activity01
+{
+    public class MainClass
+    {
+        public static double NormalFontSize = 18;
+
+        public static readonly Color ButtonColor = Color.Red;
+    }
+}
